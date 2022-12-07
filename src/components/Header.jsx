b114eb57f947c3c6770d0f3e835e584f6db13293
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2022-12-07 09:09:32 -0800

    header and links

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../flicks-logo.png"
+
+export const Header = () => {
+  return (
+ <header>
+<div className="container">
+<div className="inner-content">
+<img src={logo} alt="flicks logo" />
+<ul className="nav-links">
+<li>
+ <Link to="/">Home</Link>
+    </li>
+    <li>
+<Link to="/watchlist">Watchlist</Link>
+    </li>
+    <li>
+<Link to="/watched">Watched</Link>
+    </li>
+    <li>
+<Link to="/search">Search</Link>
+    </li>
+    </ul>
+
+
+    </div>
+   </div>
+</header>
+  )
+}
